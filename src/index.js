6fb04f030e7fd5df7a9080700c129367b9014322
Kate Zhukel <zhukelkatya@gmail.com>
2018-09-17 00:06:04 +0300

    resolved function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    let res = 0;
+    while(number > 0) {
+        number = div(number,5);
+        res += number;
+    }
+    return res;
+};
+function div(x, y){
+    return (x - x % y) / y;
 }
